c92f1bbae4e30f2ac35636f8d39fe4b0a4d3d4eb
FelipeGruend <fcgruendemann@inf.ufpel.edu.br>
2018-06-06 17:13:31 -0300

    Funcionando predicao e matriz de confusao

--- redeneural.py
+++ redeneural.py
@@ -1,18 +1,37 @@
+from sklearn.neural_network import MLPClassifier as MLPC
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler  
+from sklearn.datasets import load_breast_cancer
+
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Localização do dataset
-url = "breast-cancer-wisconsin.data"
+data = load_breast_cancer()
 
-#
-names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+data.keys()
 
+data['data'].shape;
 
-breastCancerData = pd.read_csv(url, names=names)
+X = data['data']
+y = data['target']
 
-features_train = breastCancerData[['Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses']]
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-label_train = breastCancerData['Class']
+scaler = StandardScaler()
 
+scaler.fit(X_train)
 
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-print(label_train)
+clf = MLPC(hidden_layer_sizes=(100, 100, 100), max_iter=1000)
+
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+
+confusion_m = pd.DataFrame(confusion_matrix(y_test,predictions), columns=['Benigno', 'Maligno'], index=['Benigno','Maligno'])
+plt.show(sns.heatmap(confusion_m, annot=True, fmt='g')) 
